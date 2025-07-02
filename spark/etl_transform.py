5c219b47cd95dc458a165226e33aa319aa381d6c
umakanthkanth <65246708+umakanthkanth@users.noreply.github.com>
2025-07-02 20:22:24 +0530

    Update etl_transform.py

--- spark/etl_transform.py
+++ spark/etl_transform.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-def createsparksession():
+def create_spark_session():
   spark = SparkSession.builder \
         .appName("StoreSalesETL") \
         .config("spark.executor.memory", "2g") \
@@ -18,5 +18,9 @@ def load_sales_data(spark, file_path):
     df.printSchema()
     df.show(5, truncate=False)
     return df
-
+if __name__ == "__main__":
+    spark = create_spark_session()
+    file_path = "/content/store_sales.csv"  # Replace with GCS path later
+    sales_df = load_sales_data(spark, file_path)
+    sales_df.show(5)
 
